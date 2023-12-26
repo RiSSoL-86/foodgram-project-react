919c90b0f88a78e0ccc50160b027a11c9eb64888
Илья <rissol1986@yandex.ru>
2023-12-26 15:24:28 +0300

    Tests7

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import include, path
+
+
+def redoc(request):
+    """Документация API."""
+    return render(request, 'redoc.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('redoc/', redoc, name='redoc')
 ]
