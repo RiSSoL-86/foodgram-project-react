803830a2e6cf507c436fb812747b1a4ad29f296c
Илья <rissol1986@yandex.ru>
2024-01-09 12:29:38 +0300

    на ревью 2

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.db.models import F
 from django.http import FileResponse
 from djoser.views import UserViewSet
-from rest_framework import status, viewsets 
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny, IsAuthenticated
