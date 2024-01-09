977d147eb692e41b385295e7dce3d8233045247f
Илья <rissol1986@yandex.ru>
2024-01-09 12:52:45 +0300

    на ревью 2

--- backend/foodgram/constants.py
+++ backend/foodgram/constants.py
@@ -5,13 +5,9 @@ REGEX_SIGNS = RegexValidator(r'^[\w.@+-]+\Z', 'Поддерживаемые зн
 # Validators for field 'color' in Tag!
 REGEX_COLOR = RegexValidator(r'^#[0-9A-F]{6}$', 'Поддерживаемые знаки.')
 # Validators for field 'slug' in Tag!
-REGEX_SLUG = RegexValidator(r'^[-a-zA-Z0-9_]+$', 'Поддерживаемые знаки.')
-# Validators for field 'amount' in RecipeIngredient!
-REGEX_AMOUNT = RegexValidator(r'^[1-9][0-9]*$', 'Укажите значение кол-ва '
-                              'добавляемого ингридиента больше 0.')
+
 # Constants for models!
 NAME_MAX_LENGTH = 150
-EMAIL_MAX_LENGTH = 254
 TEXT_MAX_LENGTH = 200
 # Default pagination size!
 DEFAULT_PAGES_LIMIT = 6
